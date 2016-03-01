07e3d87424a9e570d4e2547c23d4adb92b486c7d
Ricardo Souza <ricardosouzaaf@gmail.com>
2016-02-29 20:47:12 -0800

    Gerando boletos em lote

--- app/controllers/lote_controller.rb
+++ app/controllers/lote_controller.rb
@@ -15,49 +15,87 @@ class LoteController < ApplicationController
   	
   	#pegando parametro do pai
 
-  	pai = params[:client]
+    mes = params[:date][:start]
+    puts "Mes: #{mes}"
+  
 
-  	puts "Cliente: #{pai}"
+    @todos = Client.all
 
-  	@cliente = Client.where(:registro_aluno => params[:client]).first
+    @mes = I18n.l(Date.today, format: "%B")
 
-  	@mes = I18n.l(Date.today, format: "%B")
+    mes_atual = Date.today.strftime('%m')
 
-  	mes_atual = Date.today.strftime('%m')
+    data = Date.today.to_time(:utc)
 
-  	data = Date.today.to_time(:utc)
+    puts data.end_of_year
 
-  	puts data.end_of_year
+    mes_atual = mes_atual.to_i
 
-  	mes_atual = mes_atual.to_i
+    year = Time.now.year
+    puts "O Mês atual é: #{mes_atual}"
 
-  	year = Time.now.year
+    vencimento = "#{year}-#{mes}-10"
+    vencimento = vencimento.to_date
+    puts "O vencimento sera: #{vencimento}"
+    
+
+    @boletos_para_impressao = []
 
 
 
+    @todos.each do |cliente_boleto|
 
-  	while mes_atual != 13 do 
+      doc_number = "#{cliente_boleto.registro_aluno}#{mes_atual}"
 
-  		puts "O Mês atual é: #{mes_atual}"
+      carne = Crediario.new(:client_id => cliente_boleto.id, :amount => 35.00, :emission => Date.today, :maturity => vencimento, :doc_number => doc_number, :printed => false, :status => false )
 
-  		vencimento = "#{year}-#{mes_atual}-10"
-  		vencimento = vencimento.to_date
-  		puts "O vencimento sera: #{vencimento}"
-  		doc_number = "#{@cliente.registro_aluno}#{mes_atual}"
-  		carne = Crediario.new(:client_id => @cliente.id, :amount => 35.00, :emission => Date.today, :maturity => vencimento, :doc_number => doc_number, :printed => false, :status => false )
+      puts carne.inspect
 
-  		puts carne.inspect
-  		
-  		carne.save
-  		mes_atual = mes_atual.to_i + 1	
+      carne.save
 
-  	end # final while
-  	if carne.save
-        
-          flash[:success] = "Carnê criado com sucesso!"
-          redirect_to :controller => 'lote', :action => 'index'	
+      last_id = Crediario.maximum('id')
+
+      @dados_boleto = Crediario.find(last_id)
+      @dados_boleto.update_attributes(:printed => true)
+      @boleto = @dados_boleto
+
+
+    ############## Montando a hash de boletos
+
+     boleto_dados = { :cedente => "APM DO COLÉGIO MILITAR",
+      :documento_cedente => "01064671000189",
+      :sacado => @dados_boleto.client.name,
+      :sacado_documento => @dados_boleto.client.cpf,
+      :valor => @dados_boleto.amount,
+      :agencia => "0911",
+      :conta_corrente => "05945",
+      :variacao => "19",
+      :byte_idt => "2",
+      :posto => "05",
+      :numero_documento => "10402",
+      :data_vencimento => @dados_boleto.maturity.to_date,
+      :data_documento =>  @dados_boleto.emission.to_date,
+      :instrucao1 => "SR. CAIXA, APÓS O VENCIMENTO NAO COBRAR MULTA, JUROS OU MORA.",
+      :instrucao2 => "SR. RESPONSÁVEL, O NAO PAGAMENTO DESTE BOLETO IMPLICARÁ A PERDA DO DESCONTO DO VALOR.",
+      :instrucao3 => "VENCIMENTO REFERENTE AO ALUNO: #{@dados_boleto.client.registro_aluno} / Turma: #{@dados_boleto.client.turma}",
+      :sacado_endereco => @dados_boleto.client.address }
+
+
+
+      boleto_sicredi = Brcobranca::Boleto::Sicredi.new(boleto_dados)
+
+      @boletos_para_impressao << boleto_sicredi
+
+    end
+
+     send_data Brcobranca::Boleto::Base.lote(@boletos_para_impressao), :filename => "Carnês_APM_Mês(#{mes}).pdf"
+
+
+
+     flash[:success] = "Boletos gerados com sucesso!"
+     #redirect_to :controller => 'lote', :action => 'index'	
        
-      end  
+    
   	
   end #final método generate
 
